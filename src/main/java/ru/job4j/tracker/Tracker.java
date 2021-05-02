0d7428f8d91523d40fc4d34ca2ceaf23021619b6
CyberfuzZ <keepthelight@icloud.com>
2021-05-02 15:35:53 +0300

    7. Метод удаления заявки Tracker.delete [#511121]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -52,4 +52,14 @@ public class Tracker {
         }
         return rsl;
     }
+
+    public boolean delete(int id) {
+        int index = indexOf(id);
+        if (index != -1) {
+            System.arraycopy(items, index + 1, items, index, size - index);
+            items[size - 1] = null;
+            size--;
+        }
+        return index != -1;
+    }
 }
