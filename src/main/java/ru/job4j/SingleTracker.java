b1ea4c139a8281e13e6513c12cb85496cdaaf696
CyberfuzZ <keepthelight@icloud.com>
2021-05-10 13:33:28 +0300

    2. Singleton [#511082]

--- src/main/java/ru/job4j/SingleTracker.java
+++ src/main/java/ru/job4j/SingleTracker.java
@@ -4,40 +4,37 @@ import ru.job4j.tracker.Item;
 import ru.job4j.tracker.Tracker;
 
 public final class SingleTracker {
-    private static Tracker tracker = null;
+    private static Tracker tracker = new Tracker();
 
     private SingleTracker() {
 
     }
 
-    private static Tracker getTracker() {
-        if (tracker == null) {
-            tracker = new Tracker();
-        }
+    public static Tracker getTracker() {
         return tracker;
     }
 
     public Item add(Item item) {
-        return getTracker().add(item);
+        return tracker.add(item);
     }
 
     public Item findById(int id) {
-        return getTracker().findById(id);
+        return tracker.findById(id);
     }
 
     public Item[] findAll() {
-        return getTracker().findAll();
+        return tracker.findAll();
     }
 
     public Item[] findByName(String key) {
-        return getTracker().findByName(key);
+        return tracker.findByName(key);
     }
 
     public boolean replace(int id, Item item) {
-        return getTracker().replace(id, item);
+        return tracker.replace(id, item);
     }
 
     public boolean delete(int id) {
-        return getTracker().delete(id);
+        return tracker.delete(id);
     }
 }
