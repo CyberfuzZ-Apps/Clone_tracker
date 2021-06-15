33ffa8579d0f05fb490fdf0dafcf8d07fd9ffc88
CyberfuzZ <keepthelight@icloud.com>
2021-06-15 11:38:21 +0300

    0. Stream API [#58166 #167864]

--- src/main/java/ru/job4j/stream/StreamExample.java
+++ src/main/java/ru/job4j/stream/StreamExample.java
@@ -8,7 +8,7 @@ import java.util.stream.Collectors;
 public class StreamExample {
     public static void main(String[] args) {
         ArrayList<Integer> arrayList = new ArrayList<>(Arrays.asList(2, 4, -3, 5, -1, -4, 6));
-        List<Integer> list = arrayList.stream().filter(x -> x >= 0)
+        List<Integer> list = arrayList.stream().filter(x -> x > 0)
                 .collect(Collectors.toList());
         System.out.println(list);
     }
